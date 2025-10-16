2a74d6371f96e016bd3b191b2638fe1df6a1f0bb
Micle <michal.polak22@gmail.com>
2025-10-16 18:28:32 +0800

    Describe the updates here

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,24 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import tagger from "@dhiwise/component-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
-  build: {
-    outDir: "build",
-    chunkSizeWarningLimit: 2000,
-  },
-  plugins: [tsconfigPaths(), react(), tagger()],
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com'],
-    onListening: () => {
-      console.log('Made by BlackFinOrca');
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: '/src/components',
+      pages: '/src/pages',
+      utils: '/src/utils',
+      styles: '/src/styles',
     },
-  }
+  },
+  build: { outDir: 'dist' },
+  // base: '/',
 });
